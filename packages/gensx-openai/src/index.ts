865f2aa2885fb69986cec177deefe860aa6355b6
Jeremy Moseley <jeremy@cortexclick.com>
2025-01-06 16:27:54 -0800

    Support passing provider name to enable multiple simultaneous OpenAI providers.

--- packages/gensx-openai/src/index.ts
+++ packages/gensx-openai/src/index.ts
@@ -14,52 +14,52 @@ declare module "gensx" {
   }
 }
 
-export const OpenAIProvider = ContextProvider((props: ClientOptions) => {
-  const openai = new OpenAI(props);
+export const OpenAIProvider = ContextProvider(
+  (props: ClientOptions & { name?: string }) => {
+    const openai = new OpenAI(props);
 
-  return { openai };
-});
-
-export const ChatCompletion = StreamComponent<ChatCompletionCreateParams>(
-  async (props) => {
-    const context = getCurrentContext();
-    const openai = context.get("openai");
-
-    if (!openai) {
-      throw new Error(
-        "OpenAI client not found in context. Please wrap your component with OpenAIProvider.",
-      );
-    }
-
-    const stream = await openai.chat.completions.create(props);
-
-    if (stream instanceof Stream) {
-      // eslint-disable-next-line no-inner-declarations
-      async function* generateTokens(): AsyncGenerator<
-        string,
-        void,
-        undefined
-      > {
-        for await (const chunk of stream as Stream<ChatCompletionChunk>) {
-          const content = chunk.choices[0]?.delta?.content;
-          if (content) {
-            yield content;
-          }
-        }
-      }
-
-      const streamable: Streamable = generateTokens();
-
-      return streamable;
-    } else {
-      // Since our stream component API must always return a streamable, wrap the result
-      const content = stream.choices[0]?.message?.content ?? "";
-      // eslint-disable-next-line no-inner-declarations, @typescript-eslint/require-await
-      async function* generateTokens() {
-        yield content;
-      }
-
-      return generateTokens();
-    }
+    return { [props.name ?? "openai"]: openai };
   },
 );
+
+export const ChatCompletion = StreamComponent<
+  ChatCompletionCreateParams & { providerName?: string }
+>(async (props) => {
+  const context = getCurrentContext();
+  const openai = context.get(props.providerName ?? "openai") as
+    | OpenAI
+    | undefined;
+
+  if (!openai) {
+    throw new Error(
+      "OpenAI client not found in context. Please wrap your component with OpenAIProvider.",
+    );
+  }
+
+  const stream = await openai.chat.completions.create(props);
+
+  if (stream instanceof Stream) {
+    // eslint-disable-next-line no-inner-declarations
+    async function* generateTokens(): AsyncGenerator<string, void, undefined> {
+      for await (const chunk of stream as Stream<ChatCompletionChunk>) {
+        const content = chunk.choices[0]?.delta?.content;
+        if (content) {
+          yield content;
+        }
+      }
+    }
+
+    const streamable: Streamable = generateTokens();
+
+    return streamable;
+  } else {
+    // Since our stream component API must always return a streamable, wrap the result
+    const content = stream.choices[0]?.message?.content ?? "";
+    // eslint-disable-next-line no-inner-declarations, @typescript-eslint/require-await
+    async function* generateTokens() {
+      yield content;
+    }
+
+    return generateTokens();
+  }
+});
