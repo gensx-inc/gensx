0f068f750d4d51f9082df5d668d5e1c7a63ad528
Jeremy Moseley <jeremy@gensx.com>
2025-01-31 10:32:04 -0600

    Rename to gensx

--- packages/gensx-cli/bin/run.ts
+++ packages/gensx-cli/bin/run.ts
@@ -1,13 +1,3 @@
 #!/usr/bin/env node
-import { config } from 'dotenv'
-import { bgBlue, bold, red } from 'picocolors'
-import '../src/index'
 
-config()
-
-console.log(
-  bgBlue(
-    `Welcome to ${bold(red('gensx-cli'))}!
-    Bundle and deploy GenSX workflows to the cloud.`,
-  ),
-)
+import "../src/index";
--- packages/gensx-cli/src/index.ts
+++ packages/gensx-cli/src/index.ts
@@ -3,7 +3,7 @@ import { Command } from "commander";
 import { login } from "./commands/login";
 
 const program = new Command()
-  .name("gensx-cli")
+  .name("gensx")
   .description("CLI tool for getting started with GenSX")
   .version("0.1.0");
 
