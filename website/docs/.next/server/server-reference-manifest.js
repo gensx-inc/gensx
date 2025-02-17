1ad880df2cd8780486cf14e094d9eaba8d0f5c78
Dan Hernandez <33551757+danfhernandez@users.noreply.github.com>
2025-02-17 17:43:31 -0500

    Remove next folder

--- website/docs/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"3XoqnnAYycy1V2rKGAJOfiqt3rMpS8STP/p2bsWtlU0=\"\n}"
--- website/docs/.next/transform.js
+++
@@ -1,6 +0,0 @@
-const CHUNK_PUBLIC_PATH = "transform.js";
-const runtime = require("./build/chunks/[turbopack]_runtime.js");
-runtime.loadChunk("build/chunks/[root of the server]__7ac3af._.js");
-runtime.loadChunk("build/chunks/postcss_config_mjs_transform_ts_0b0d12._.js");
-runtime.getOrInstantiateRuntimeModule("[turbopack-node]/globals.ts [postcss] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[project]/website/docs/postcss.config.mjs/transform.ts { CONFIG => \\\"[project]/website/docs/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
