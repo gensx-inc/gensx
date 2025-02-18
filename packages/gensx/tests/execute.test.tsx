c85b256a205051fa00d5f8a337fedb1780f56c6e
Jeremy Moseley <jeremy@gensx.com>
2025-02-18 15:29:13 -0800

    Add types test.

--- packages/gensx/tests/execute.test.tsx
+++ packages/gensx/tests/execute.test.tsx
@@ -3,15 +3,29 @@ import { setTimeout } from "timers/promises";
 import { expect, suite, test } from "vitest";
 
 import { gsx, Streamable } from "../src";
+import { executeWithCheckpoints } from "./utils/executeWithCheckpoints";
+
+type Assert<T, U> =
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
+  (<V>() => V extends T ? 1 : 2) extends <V>() => V extends U ? 1 : 2
+    ? true
+    : { error: "Types are not equal"; type1: T; type2: U };
 
 suite("execute", () => {
-  const WorkflowComponent = gsx.Component("test", () => "hello");
+  const WorkflowComponent = gsx.Component("test", async () => {
+    await setTimeout(0);
+    return "hello";
+  });
 
   suite("workflow", () => {
     test("can execute a workflow", async () => {
       const workflow = gsx.workflow("test", WorkflowComponent);
       const result = await workflow.run({});
+
+      const assertReturnType: Assert<typeof result, string> = true;
+
       expect(result).toBe("hello");
+      expect(assertReturnType).toBe(true);
     });
 
     test("can execute a workflow with a stream component", async () => {
@@ -38,14 +52,37 @@ suite("execute", () => {
       }
       expect(result).toBe("hello");
 
+      const assertIteratorReturnType: Assert<typeof iterator, Streamable> =
+        true;
+      expect(assertIteratorReturnType).toBe(true);
+
       const stringResult: string = await workflow.run({
         stream: false,
         foo: "hello",
       });
       expect(stringResult).toBe("hello");
 
+      const assertStringResultReturnType: Assert<typeof stringResult, string> =
+        true;
+      expect(assertStringResultReturnType).toBe(true);
+
       const stringResult2: string = await workflow.run({ foo: "hello" });
       expect(stringResult2).toBe("hello");
+
+      const assertStringResult2ReturnType: Assert<
+        typeof stringResult2,
+        string
+      > = true;
+      expect(assertStringResult2ReturnType).toBe(true);
+    });
+
+    test("can run workflows in parallel", async () => {
+      const result1 = executeWithCheckpoints(WorkflowComponent);
+      const result2 = executeWithCheckpoints(WorkflowComponent);
+
+      const [r1, r2] = await Promise.all([result1, result2]);
+      expect(r1).toBe("hello");
+      expect(r2).toBe("hello");
     });
   });
 
