45dbe3ccdbf2adf4a8a07a3d476a4e86b763f7e8
Jeremy Moseley <jeremy@gensx.com>
2025-02-21 14:55:20 -0800

    Remove nextra favicon glyph.

--- website/docs/src/app/layout.tsx
+++ website/docs/src/app/layout.tsx
@@ -43,7 +43,7 @@ export default async function RootLayout({ children }) {
   const pageMap = await getPageMap("/docs");
   return (
     <html lang="en" dir="ltr" suppressHydrationWarning>
-      <Head faviconGlyph="✦" />
+      <Head />
       <body className={`${figtree.variable}`}>
         <Layout
           navbar={navbar}
