27b18f12027d04c7fec84a38dcccf13d476d3ccd
evanboyle <io.evanboyle@gmail.com>
2025-02-21 18:10:50 -0800

    push commit to branch

--- examples/self-modifying-code/workspace.ts
+++ examples/self-modifying-code/workspace.ts
@@ -54,7 +54,7 @@ function deserializeContext(json: string): AgentContext {
   });
 }
 
-async function runCommand(
+export async function runCommand(
   cmd: string,
   args: string[],
   cwd: string,
