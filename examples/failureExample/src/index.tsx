69a33c78e8187b2aeb659319d62941b1d3c8bebd
Dan Hernandez <33551757+danfhernandez@users.noreply.github.com>
2025-02-27 10:39:18 -0500

    Failure example

---
+++ examples/failureExample/src/index.tsx
@@ -0,0 +1,53 @@
+import { ChatCompletion } from "@gensx/openai";
+import { gsx } from "gensx";
+
+interface RespondProps {
+  userInput: string;
+}
+type RespondOutput = string;
+
+const Respond = gsx.Component<RespondProps, RespondOutput>(
+  "Respond",
+  ({ userInput }) => (
+    <ChatCompletion
+      model="gpt-4o-mini"
+      messages={[
+        {
+          role: "system",
+          content: "You are a helpful assistant. Respond to the user's input.",
+        },
+        { role: "user", content: userInput },
+      ]}
+    />
+  ),
+);
+
+const ErrorComponent = gsx.Component<
+  { message: string; timeout: number },
+  string
+>("ErrorComponent", async ({ message, timeout }) => {
+  await new Promise((resolve) => setTimeout(resolve, timeout));
+  throw new Error(message);
+});
+
+const WorkflowComponent = gsx.Component<{ userInput: string }, string>(
+  "Workflow",
+  ({ userInput }) => (
+    <>
+      <ErrorComponent timeout={10000} message="Test error message" />
+      <ErrorComponent timeout={1} message="Test error message" />
+      <ErrorComponent timeout={1000} message="Test error message" />
+    </>
+  ),
+);
+
+const workflow = gsx.Workflow("MyGSXWorkflow", WorkflowComponent);
+
+const result = await workflow.run(
+  {
+    userInput: "Hi there! Say 'Hello, World!' and nothing else.",
+  },
+  { printUrl: true },
+);
+
+// console.log(result);
