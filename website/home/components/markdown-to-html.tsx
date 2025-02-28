30b335c4a84e72545cee6d9da02f35a05565a52c
Dan Hernandez <33551757+danfhernandez@users.noreply.github.com>
2025-02-28 00:08:11 -0500

    Fixed up some formatting

--- website/home/components/markdown-to-html.tsx
+++ website/home/components/markdown-to-html.tsx
@@ -11,7 +11,24 @@ interface CodeComponentProps extends React.HTMLAttributes<HTMLElement> {
   className?: string;
   children?: React.ReactNode;
 }
-// This
+
+// Custom component to render the styled image using CSS instead of nested divs
+const StyledImage = (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
+  <span className="block relative my-8 w-full before:content-[''] before:absolute before:left-0 before:top-0 before:w-[30px] before:h-[1px] before:bg-gray-800 before:z-10 after:content-[''] after:absolute after:left-0 after:top-0 after:w-[1px] after:h-[30px] after:bg-gray-800 after:z-10">
+    <span className="block relative before:content-[''] before:absolute before:right-0 before:top-0 before:w-[30px] before:h-[1px] before:bg-gray-800 before:z-10 after:content-[''] after:absolute after:right-0 after:top-0 after:w-[1px] after:h-[30px] after:bg-gray-800 after:z-10">
+      <span className="block relative before:content-[''] before:absolute before:left-0 before:bottom-0 before:w-[30px] before:h-[1px] before:bg-gray-800 before:z-10 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-[1px] after:h-[30px] after:bg-gray-800 after:z-10">
+        <span className="block relative before:content-[''] before:absolute before:right-0 before:bottom-0 before:w-[30px] before:h-[1px] before:bg-gray-800 before:z-10 after:content-[''] after:absolute after:right-0 after:bottom-0 after:w-[1px] after:h-[30px] after:bg-gray-800 after:z-10">
+          <span className="block relative border-t border-b border-gray-200">
+            <span className="block relative border-l border-r border-gray-200 my-[30px]">
+              <img {...props} className="border-0 py-4 px-0 w-full" />
+            </span>
+          </span>
+        </span>
+      </span>
+    </span>
+  </span>
+);
+
 export default function MarkdownToHTML({
   markdown,
   className,
@@ -20,12 +37,38 @@ export default function MarkdownToHTML({
   className?: string;
 }) {
   return (
-    <div className="[&_pre]:overflow-x-auto [&_pre]:bg-white [&_pre]:rounded-[0px] [&_pre]:max-w-[95vw] md:[&_pre]:max-w-[85vw] [&_pre]:mb-0">
+    <div className="[&_pre]:overflow-x-auto [&_pre]:bg-white [&_pre]:rounded-[0px] [&_pre]:mb-0">
       <ReactMarkdown
         remarkPlugins={[remarkGfm]}
         rehypePlugins={[rehypeHighlight]}
         className={className}
         components={{
+          // Override paragraph rendering
+          p(props) {
+            const { children, ...rest } = props;
+
+            // Check if children contains only an image
+            const childArray = React.Children.toArray(children);
+            if (childArray.length === 1) {
+              const onlyChild = childArray[0];
+              if (React.isValidElement(onlyChild) && onlyChild.type === "img") {
+                // Return the StyledImage component directly with properly typed props
+                return (
+                  <StyledImage
+                    {...(onlyChild.props as React.ImgHTMLAttributes<HTMLImageElement>)}
+                  />
+                );
+              }
+            }
+
+            return <p {...rest}>{children}</p>;
+          },
+
+          // This will only be used for images not directly in paragraphs
+          img(props) {
+            return <StyledImage {...props} />;
+          },
+
           code(props: CodeComponentProps) {
             const { inline, children, className, ...rest } = props;
             const language = className?.replace("hljs language-", "");
@@ -39,21 +82,31 @@ export default function MarkdownToHTML({
             }
 
             return (
-              <div className="relative border border-gray-200 mt-4 mb-1 pb-0">
-                {language && (
-                  <div className="absolute right-4 top-2 text-xs font-mono text-gray-800 z-10">
-                    {language}
-                  </div>
-                )}
-                <pre className="bg-white rounded-[0px] w-full px-2 !pt-2 !pb-1">
-                  <code
-                    {...rest}
-                    className={`${className} text-xs md:text-sm leading-relaxed pb-0 block`}
-                  >
-                    {children}
-                  </code>
-                </pre>
-              </div>
+              <span className="block relative mt-4 mb-1 w-full before:content-[''] before:absolute before:left-0 before:top-0 before:w-[30px] before:h-[1px] before:bg-gray-800 before:z-10 after:content-[''] after:absolute after:left-0 after:top-0 after:w-[1px] after:h-[30px] after:bg-gray-800 after:z-10">
+                <span className="block relative before:content-[''] before:absolute before:right-0 before:top-0 before:w-[30px] before:h-[1px] before:bg-gray-800 before:z-10 after:content-[''] after:absolute after:right-0 after:top-0 after:w-[1px] after:h-[30px] after:bg-gray-800 after:z-10">
+                  <span className="block relative before:content-[''] before:absolute before:left-0 before:bottom-0 before:w-[30px] before:h-[1px] before:bg-gray-800 before:z-10 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-[1px] after:h-[30px] after:bg-gray-800 after:z-10">
+                    <span className="block relative before:content-[''] before:absolute before:right-0 before:bottom-0 before:w-[30px] before:h-[1px] before:bg-gray-800 before:z-10 after:content-[''] after:absolute after:right-0 after:bottom-0 after:w-[1px] after:h-[30px] after:bg-gray-800 after:z-10">
+                      <span className="block relative border-t border-b border-gray-200">
+                        <span className="block relative border-l border-r border-gray-200 my-[30px]">
+                          {language && (
+                            <span className="absolute right-4 top-3 text-xs font-mono text-gray-800 z-10">
+                              {language}
+                            </span>
+                          )}
+                          <pre className="bg-white w-full py-4 px-0 border-0">
+                            <code
+                              {...rest}
+                              className={`${className} text-xs md:text-sm leading-relaxed pb-0 block`}
+                            >
+                              {children}
+                            </code>
+                          </pre>
+                        </span>
+                      </span>
+                    </span>
+                  </span>
+                </span>
+              </span>
             );
           },
         }}
