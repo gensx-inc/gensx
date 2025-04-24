f897853651bfc2fbf00b30c8b919ad5d8d5abaff
Derek Legenzoff <dereklegenzoff@gmail.com>
2025-04-23 17:02:00 -0700

    making package.json resolution more flexible

--- packages/gensx-core/src/utils/user-agent.ts
+++ packages/gensx-core/src/utils/user-agent.ts
@@ -1,13 +1,30 @@
-import { readFileSync } from "node:fs";
+import { existsSync, readFileSync } from "node:fs";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
+let version = "";
 const dirname = path.dirname(fileURLToPath(import.meta.url));
-const rootDir = path.resolve(dirname, "..", "..", "..");
 
-const localPackageJson = JSON.parse(
-  readFileSync(path.join(rootDir, "package.json"), "utf8"),
-) as { version: string };
+let rootDir = path.resolve(dirname, "..", "..", "..");
+let packageJsonPath = path.join(rootDir, "package.json");
+if (existsSync(packageJsonPath)) {
+  const localPackageJson = JSON.parse(
+    readFileSync(packageJsonPath, "utf8"),
+  ) as { version: string };
+  version = localPackageJson.version;
+} else {
+  // if the first check fails, try two levels up instead of three
+  rootDir = path.resolve(dirname, "..", "..");
+  packageJsonPath = path.join(rootDir, "package.json");
+  if (existsSync(packageJsonPath)) {
+    const localPackageJson = JSON.parse(
+      readFileSync(packageJsonPath, "utf8"),
+    ) as { version: string };
+    version = localPackageJson.version;
+  } else {
+    console.error("Error trying to get version from package.json");
+  }
+}
 
-export const VERSION = localPackageJson.version;
+export const VERSION = version;
 export const USER_AGENT = `@gensx/core v${VERSION}`;
