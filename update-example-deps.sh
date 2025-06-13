4f3f9a9ebe1ead55511b8645bf9f405c15211e09
Jeremy Moseley <jeremy@gensx.com>
2025-06-13 11:45:24 -0700

    Improve update script.

--- update-example-deps.sh
+++ update-example-deps.sh
@@ -3,8 +3,8 @@
 # Iterate over each example and update all @gensx packages to the latest version
 for example in examples/*; do
   if [ -d "$example" ]; then
-    cd $example
+    pushd "$example"
     pnpm update @gensx/core @gensx/storage @gensx/vercel-ai @gensx/openai @gensx/anthropic
-    cd ../..
+    popd
   fi
 done
