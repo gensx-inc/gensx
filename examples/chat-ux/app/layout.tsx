01c1c00e85e599517b6f4e202bb3088e8705d286
Derek Legenzoff <dereklegenzoff@gmail.com>
2025-06-19 15:06:54 -0700

    fixing build error

--- examples/chat-ux/app/layout.tsx
+++ examples/chat-ux/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Suspense } from "react";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50`}
       >
-        {children}
+        <Suspense
+          fallback={
+            <div className="flex h-screen items-center justify-center">
+              Loading...
+            </div>
+          }
+        >
+          {children}
+        </Suspense>
       </body>
     </html>
   );
